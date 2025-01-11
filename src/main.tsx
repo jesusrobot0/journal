d7cf08a8dc3b163d3daec40fa34bb7be6ee317e6
jesusrobot0 <jesusrobot9000@icloud.com>
2025-01-11 00:14:36 -0600

    Chore: Añade MUI

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { JournalApp } from "./JournalApp";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
